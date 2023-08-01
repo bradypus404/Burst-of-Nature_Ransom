6c275729a552fb99dd2b72bb4b6ab1e12f7ba26a
Foliv0ra <bradypus404@gmail.com>
2023-08-01 22:03:17 +0900

    start-Loki_foliv0ra_0.2v

--- src/start_Loki.rs
+++ src/start_Loki.rs
@@ -1,65 +1,67 @@
+// use std::io;
+// use std::path::Path;
+// use winreg::enums::*;
+// use winreg::RegKey;
+
+// fn main() -> io::Result<()> {
+
+//     println!("Reading some system info...");
+//     let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
+//     let cur_ver = hklm.open_subkey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run")?;
+//     for (name, value) in cur_ver.enum_values().map(|x| x.unwrap()) {
+//         println!("{} = {:?}", name, value);
+//     }
+//     println!("------------------------------------------------------");
+
+//     let hkcu = RegKey::predef(HKEY_CURRENT_USER);
+//     let path = Path::new("SOFTWARE\\Microsoft\\Windows\\CurrentVersion").join("Run");
+//     let (_key, _disp) = hkcu.create_subkey(&path)?;
+
+//     println!("{}", path.display());
+
+//     _key.set_value("ThisIsLoki", &"C:\\Users\\brady\\VScode\\rust\\LokiLoki.exe")?;
+
+//     Ok(())
+// }
+
 use std::io;
 use std::path::Path;
-use winreg::enums::*;
 use winreg::RegKey;
+use winreg::enums::*;
 
-fn main() -> io::Result<()> {
+fn read_registry_values() -> io::Result<()> {
     println!("Reading some system info...");
     let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
-    let cur_ver = hklm.open_subkey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion")?;
-    let pf: String = cur_ver.get_value("ProgramFilesDir")?;
-    let dp: String = cur_ver.get_value("DevicePath")?;
-    println!("ProgramFiles = {}\nDevicePath = {}", pf, dp);
-    let info = cur_ver.query_info()?;
-    println!("info = {:?}", info);
-    let mt = info.get_last_write_time_system();
-    println!(
-        "last_write_time as windows_sys::Win32::Foundation::SYSTEMTIME = {}-{:02}-{:02} {:02}:{:02}:{:02}",
-        mt.wYear, mt.wMonth, mt.wDay, mt.wHour, mt.wMinute, mt.wSecond
-    );
-// test
-    // enable `chrono` feature on `winreg` to make this work
-    // println!(
-    //     "last_write_time as chrono::NaiveDateTime = {}",
-    //     info.get_last_write_time_chrono()
-    // );
-
-    println!("And now lets write something...");
-    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
-    let path = Path::new("Software").join("WinregRsExample1");
-    let (key, disp) = hkcu.create_subkey(&path)?;
-
-    match disp {
-        REG_CREATED_NEW_KEY => println!("A new key has been created"),
-        REG_OPENED_EXISTING_KEY => println!("An existing key has been opened"),
+    let cur_ver = hklm.open_subkey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run")?;
+    for (name, value) in cur_ver.enum_values().map(|x| x.unwrap()) {
+        println!("{} = {:?}", name, value);
     }
+    println!("------------------------------------------------------");
 
-    key.set_value("TestSZ", &"written by Rust")?;
-    let sz_val: String = key.get_value("TestSZ")?;
-    key.delete_value("TestSZ")?;
-    println!("TestSZ = {}", sz_val);
+    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
+    let path = Path::new("SOFTWARE\\Microsoft\\Windows\\CurrentVersion").join("Run");
+    let (_key, _disp) = hkcu.create_subkey(&path)?;
 
-    key.set_value("TestMultiSZ", &vec!["written", "by", "Rust"])?;
-    let multi_sz_val: Vec<String> = key.get_value("TestMultiSZ")?;
-    key.delete_value("TestMultiSZ")?;
-    println!("TestMultiSZ = {:?}", multi_sz_val);
+    println!("{}", path.display());
 
-    key.set_value("TestDWORD", &1234567890u32)?;
-    let dword_val: u32 = key.get_value("TestDWORD")?;
-    println!("TestDWORD = {}", dword_val);
+    Ok(())
+}
 
-    key.set_value("TestQWORD", &1234567891011121314u64)?;
-    let qword_val: u64 = key.get_value("TestQWORD")?;
-    println!("TestQWORD = {}", qword_val);
+fn add_registry_value() -> io::Result<()> {
+    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
+    let path = Path::new("SOFTWARE\\Microsoft\\Windows\\CurrentVersion").join("Run");
+    let (_key, _disp) = hkcu.create_subkey(&path)?;
 
-    key.create_subkey("sub\\key")?;
-    hkcu.delete_subkey_all(&path)?;
+    println!("{}", path.display());
 
-    println!("Trying to open nonexistent key...");
-    hkcu.open_subkey(&path).unwrap_or_else(|e| match e.kind() {
-        io::ErrorKind::NotFound => panic!("Key doesn't exist"),
-        io::ErrorKind::PermissionDenied => panic!("Access denied"),
-        _ => panic!("{:?}", e),
-    });
+    _key.set_value("ThisIsLoki", &"C:\\Users\\brady\\VScode\\rust\\LokiLoki.exe")?;
+
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    read_registry_values()?;
+    add_registry_value()?;
+
     Ok(())
 }
