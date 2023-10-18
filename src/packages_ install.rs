6bb34b57a0959807dca85900cf11f6bf68b9e289
rkxsaos <139982991+rkxsaos@users.noreply.github.com>
2023-10-18 19:29:14 +0900

    Add files via upload

---
+++ src/packages_ install.rs
@@ -0,0 +1,33 @@
+msys2를 설치 후
+
+설치 명령어
+pacman -S [패키지 이름]
+
+설치할 패키지
+gcc 
+mingw-w64-ucrt-x86_64-gcc 
+mingw-w64-x86_64-atk 
+mingw-w64-x86_64-gtk3 
+mingw-w64-x86_64-gtk4
+mingw-w64-x86_64-pkg-config 
+mingw-w64-x86_64-rust
+
+path에 추가할 경로
+export PATH=$PATH:/mingw64/bin
+
+패키지 확인 명령어
+pacman -Qe
+
+끝난 후
+base 2022.06-1
+filesystem 2023.02.07-1
+gcc 13.2.0-2
+mingw-w64-ucrt-x86_64-gcc 13.2.0-2
+mingw-w64-x86_64-atk 2.50.0-1
+mingw-w64-x86_64-gtk3 3.24.38.r43.g0f717ca-1
+mingw-w64-x86_64-gtk4 4.12.3-1
+mingw-w64-x86_64-pkgconf 1~2.0.3-2
+mingw-w64-x86_64-rust 1.73.0-1
+msys2-runtime 3.4.9-2
+
+
